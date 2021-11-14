7f8374a02b2bd8dc774c9c4bcee65c56d5783768
alpheratz0 <alpheratz99@protonmail.com>
2021-11-14 10:38:32 -0300

    update method signatures

--- examples/roombase.ts
+++ examples/roombase.ts
@@ -1,23 +1,34 @@
-import { Room, Scores, Player } from '../src/index'
+import { Room, Scores, Player, TeamID } from 'haxjs';
 
 class RoomBase extends Room {
-    onPlayerJoin (player: Player): void {}
-    onPlayerLeave (player: Player): void {}
-    onTeamVictory (scores: Scores): void {}
-    onPlayerChat (player: Player, message: string): void {}
-    onPlayerBallKick (player: Player): void {}
-    onTeamGoal (team: number): void {}
-    onGameStart (byPlayer: Player): void {}
-    onGameStop (byPlayer: Player): void {}
-    onPlayerAdminChange (changedPlayer: Player, byPlayer: Player): void {}
-    onPlayerTeamChange (changedPlayer: Player, byPlayer: Player): void {}
-    onPlayerKicked (kickedPlayer: Player, reason: string, ban: boolean, byPlayer: Player): void {}
-    onGameTick (): void {}
-    onGamePause (byPlayer: Player): void {}
-    onGameUnpause (byPlayer: Player): void {}
-    onPositionsReset (): void {}
-    onPlayerActivity (player: Player): void {}
-    onStadiumChange (newStadiumName: string, byPlayer: Player): void {}
-    onRoomLink (url: string): void {}
-    onKickRateLimitSet (min: number, rate: number, burst: number, byPlayer: Player): void {}
+    onPlayerJoin(player: Player): void {}
+    onPlayerLeave(player: Player): void {}
+    onTeamVictory(scores: Scores): void {}
+    onPlayerChat(player: Player, message: string): void {}
+    onPlayerBallKick(player: Player): void {}
+    onTeamGoal(team: TeamID): void {}
+    onGameStart(byPlayer: Player | null): void {}
+    onGameStop(byPlayer: Player | null): void {}
+    onPlayerAdminChange(changedPlayer: Player, byPlayer: Player | null): void {}
+    onPlayerTeamChange(changedPlayer: Player, byPlayer: Player | null): void {}
+    onPlayerKicked(
+        kickedPlayer: Player,
+        reason: string,
+        ban: boolean,
+        byPlayer: Player | null
+    ): void {}
+    onGameTick(): void {}
+    onGamePause(byPlayer: Player | null): void {}
+    onGameUnpause(byPlayer: Player | null): void {}
+    onPositionsReset(): void {}
+    onPlayerActivity(player: Player): void {}
+    onStadiumChange(newStadiumName: string, byPlayer: Player | null): void {}
+    onRoomLink(url: string): void {}
+    onKickRateLimitSet(
+        min: number,
+        rate: number,
+        burst: number,
+        byPlayer: Player | null
+    ): void {}
 }
+
