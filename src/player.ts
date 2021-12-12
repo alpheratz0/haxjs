ee397d06123f46febaa68a1ccdf2d470c616088c
alpheratz0 <alpheratz99@protonmail.com>
2021-12-12 08:01:35 -0300

    update possible null properties on player interface

--- src/player.ts
+++ src/player.ts
@@ -1,7 +1,7 @@
 import { Position } from './position';
 
 export interface Player {
-    /** The id of the player, each player that joins the room 
+    /** The id of the player, each player that joins the room
      * gets a unique id that will never change. */
     id: number;
 
@@ -14,18 +14,17 @@ export interface Player {
     /** Whether the player has admin rights. */
     admin: boolean;
 
-    /** The player's position in the field, 
+    /** The player's position in the field,
      * if the player is not in the field the value will be null. */
-    position: Position;
+    position: Position | null;
 
     /** The player's public ID.
      ** Can be null if the ID validation fails.
      ** This property is only set in the onPlayerJoin event. */
-    auth: string;
+    auth: string | null;
 
     /** A string that uniquely identifies the player's connection,
      * if two players join using the same network this string will be equal.
      ** This property is only set in the onPlayerJoin event. */
     conn: string;
 }
-
