0937871246263958a0a69255482a01d9c342481b
alpheratz0 <alpheratz99@protonmail.com>
2022-03-10 15:45:46 -0300

    docs: update README

---
+++ examples/main.ts
@@ -0,0 +1,44 @@
+import { Room, Player, World } from 'haxjs';
+
+class BasicRoom extends Room {
+	onPlayerJoin(player: Player): void {
+		this.setPlayerAdmin(player.id, true);
+		this.sendAnnouncement(
+			`welcome ${player.name}!`,
+			player.id,
+			0xffff00,
+			'bold',
+			2
+		);
+	}
+
+	onPlayerChat(player: Player, message: string): void {
+		this.sendAnnouncement(
+			`${player.name}[${player.id}]: ${message}`,
+			null,
+			0xffffff,
+			null,
+			1
+		);
+	}
+
+	onRoomLink(url: string): void {
+		console.log('\n' + url);
+	}
+}
+
+const main = () => {
+	const room = new BasicRoom({
+		roomName: 'Basic Room',
+		password: undefined,
+		maxPlayers: 10,
+		public: false,
+		geo: World.UY,
+		noPlayer: true,
+		token: 'thr1.AAAAAGGQ_HvNuZLqigGpEg.vXMHp16ULVE'
+	});
+
+	room.start();
+};
+
+main();
